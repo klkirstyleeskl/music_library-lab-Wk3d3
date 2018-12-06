8cf408bbc1e203a76c7bd24cb8a536d10538fcc6
klkirstyleeskl <k.lees@btinternet.com>
2018-12-06 09:12:07 +0000

    passes all of MVP

--- db/console.rb
+++ db/console.rb
@@ -2,17 +2,32 @@ require("pry")
 require_relative('../models/artist')
 require_relative('../models/album')
 
-# Artist.delete_all()
+Album.delete_all()
+Artist.delete_all()
 
-artist1 = Artist.new({ 'name' => 'Michael Jackson'})
+artist1 = Artist.new({ 'name' => 'FleetWood Mac'})
 artist1.save
 
 album1 = Album.new({
-    'name' => 'Thriller',
-    'genre' => 'Pop',
+    'title' => 'Tusk',
+    'genre' => 'Rock',
     'artist_id' => artist1.id
   })
 album1.save
+
+album2 = Album.new({
+    'title' => 'Tango in the night',
+    'genre' => 'Rock',
+    'artist_id' => artist1.id
+  })
+album2.save
+
+album3 = Album.new({
+    'title' => 'Rumours',
+    'genre' => 'Rock',
+    'artist_id' => artist1.id
+  })
+album3.save
 
 binding.pry
 nil
