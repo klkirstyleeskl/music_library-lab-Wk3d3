390cb9d8cfdbe96d1a674dd80b8473356f5e06d9
klkirstyleeskl <k.lees@btinternet.com>
2018-12-06 09:12:51 +0000

    passes all of MVP

--- models/album.rb
+++ models/album.rb
@@ -1,23 +1,30 @@
 require('pg')
 require_relative('../db/sql_runner')
+require_relative('../models/artist')
 
 class Album
 
   attr_reader(:id)
-  attr_accessor(:name, :genre, :artist_id)
+  attr_accessor(:title, :genre, :artist_id)
 
 def initialize(options)
-  @name = options[ 'name']
+  @title = options[ 'title']
   @genre = options[ 'genre']
   @id = options['id'].to_i if options['id']
   @artist_id = options['artist_id'].to_i
   # this adds the foreign key above
 end
 
+def self.delete_all()
+  sql = "DELETE FROM albums"
+  result = SqlRunner.run(sql)
+end
+
+
 def save()
   sql = "INSERT INTO albums
     (
-    name,
+    title,
     genre,
     artist_id
     )
@@ -26,8 +33,37 @@ def save()
     $1, $2, $3
     )
     RETURNING *"
-    values = [@name, @genre, @artist_id]
+    values = [@title, @genre, @artist_id]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+end
+
+def self.all()
+  sql = "SELECT * FROM albums"
+  albums = SqlRunner.run(sql)
+  return albums.map { |customer| Album.new(album) }
+end
+
+def artist()
+  sql = "SELECT * FROM artists WHERE id = $1"
+  values = [@artist_id]
+  results = SqlRunner.run(sql, values)
+  artist_hash = results[0]
+  return Artist.new( artist_hash )
+end
+
+
+def update()
+  sql = "UPDATE albums
+  SET
+  (
+    title
+  ) =
+  (
+    $1
+  )
+  WHERE id = $2"
+  values = [@title, @id]
+  result = SqlRunner.run(sql, values)
 end
 
 
--- models/artist.rb
+++ models/artist.rb
@@ -1,5 +1,6 @@
 require('pg')
 require_relative('../db/sql_runner')
+require_relative('../models/album')
 
 class Artist
 
@@ -10,6 +11,11 @@ attr_accessor(:name)
   def initialize(options)
   @id = options['id'].to_i if options['id']
   @name = options['name']
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM artists"
+    result = SqlRunner.run(sql)
   end
 
   def save()
@@ -26,6 +32,44 @@ attr_accessor(:name)
       @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
+  def albums()
+    sql = "SELECT * FROM albums WHERE artist_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    return results.map {|order| Album.new(order)}
+  end
+
+# unsure how to test that this works// use of function 
+  def self.find(id)
+    sql = "SELECT * FROM artists
+    WHERE id = $1"
+    values = [id]
+    results =  SqlRunner.run(sql, values)
+    artist_hash = results.first
+    artist = Artist.new(artist_hash)
+    return artist
+  end
+
+
+  def self.all()
+    sql = "SELECT * FROM artists"
+    artists = SqlRunner.run(sql)
+    return artists.map { |customer| Artist.new(artist) }
+  end
+
+  def update()
+    sql = "UPDATE artists
+    SET
+    (
+      name
+    ) =
+    (
+      $1
+    )
+    WHERE id = $2"
+    values = [@name, @id]
+    result = SqlRunner.run(sql, values)
+  end
 
 
 end
